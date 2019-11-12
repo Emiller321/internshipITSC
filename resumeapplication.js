b8a45e94b56c2dfcd9bdceb12362d6ccb84115cb
Eric Miller <54557245+Emiller321@users.noreply.github.com>
2019-11-12 08:13:20 -0500

    Put $http

--- resumeapplication.js
+++ resumeapplication.js
@@ -1,4 +1,4 @@
-   angular.module('myApp', []).controller('formCtrl',function($scope)
+   angular.module('myApp', []).controller('formCtrl',function($scope, $http)
    {
        $scope.submit = function(user){
              console.log(user);
